c7af0a98ccfeb05f87fcec8311fe3ff261e37c58
Stefan Sundseth <stefan.sundseth@accenturefederal.com>
2020-01-05 10:42:30 -0500

    Initial commit

---
+++ downloader.py
@@ -0,0 +1,28 @@
+from urllib.parse import urlencode
+
+import requests
+from lxml import etree
+
+
+def download_file(file_url):
+    download_folder = "C:/Users/stefan.sundseth/edgar/data/"
+    base_url = "https://www.sec.gov/Archives/"
+
+    resp = requests.get(f"{base_url}{file_url}", verify=False)
+
+    file_name = file_url.split("/")[-1]
+
+    save_path = download_folder + file_name
+
+    with open(save_path, "w", encoding="utf-8") as f:
+        f.write(resp.text)
+
+
+
+
+with open("13fAll.txt") as f:
+    lines = [line.rstrip('\n') for line in f]
+
+for line in lines:
+    print(f"Downloading file: {line}")
+    download_file(line)
---
+++ run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+	echo "Enter year and quarter:"
+	echo "./run.sh 2019 QTR4"
+	exit 1
+fi
+
+YEAR=$1
+QTR=$2
+URL="https://www.sec.gov/Archives/edgar/full-index/$YEAR/$QTR/form.idx"
+
+echo "Getting index from $URL"
+curl $URL > form.idx
+
+echo ""
+echo "Grepping 13F-HR from index"
+grep 13F-HR form.idx | awk '{print $NF}' > 13fAll.txt
+CT=$(wc -l 13fAll.txt)
+echo "	$CT forms found"
+echo ""
+
+
+echo "Running python to download forms"
+python downloader.py
+
+echo "Completed successfully"
