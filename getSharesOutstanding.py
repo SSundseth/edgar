6945278bb913289c6d454827b30677622742b1b2
Stefan Sundseth <stefan.sundseth@accenturefederal.com>
2020-02-14 09:40:08 -0500

    Add stocklist backup and market cap

--- getSharesOutstanding.py
+++ getSharesOutstanding.py
@@ -6,7 +6,7 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
 def string_to_int(string):
-    multipliers = {'K':1000, 'M':1000000, 'B':1000000000}
+    multipliers = {'K':1000, 'M':1000000, 'B':1000000000, 'T':1000000000000}
     if string[-1].isdigit(): # check if no suffix
         return int(string)
     mult = multipliers[string[-1]] # look up suffix to get multiplier
@@ -17,7 +17,10 @@ with open("stockList") as tickerList:
     tickers = [line.rstrip('\n').split(',') for line in tickerList]
 
 
-for line in tickers:
+numTickers = len(tickers)
+
+for i in range(numTickers):
+    line = tickers[i]
     ticker = line[0]
     cusip = line[1]
     print("Ticker: "+ticker)
@@ -34,7 +37,25 @@ for line in tickers:
             total = string_to_int(lastval)
             print(total)
 
-            write_line = f"{cusip},{total},{ticker}\n"
+            
+            url=f"https://www.marketcaphistory.com/{ticker.lower()}"
+            print(url)
+            response = requests.get(url, verify=False)
+            if response.status_code == 200:
+                soup = BeautifulSoup(response.text, 'lxml')
+                table = soup.findAll("td", {"class":"tstyle"})
+                if len(table) > 0:
+                    lastval = table[-1].text
+                    cap = string_to_int(lastval)
+                    print(cap)
+                else:
+                    print("table length 0")
+                    total = 0
+            else:
+                print("Response code: "+str(response.status_code))
+                total = 0
+
+            write_line = f"{cusip},{total},{ticker},{cap}\n"
             with open("tickerTotals.csv", "a") as write_file:
                     write_file.write(write_line)
 
